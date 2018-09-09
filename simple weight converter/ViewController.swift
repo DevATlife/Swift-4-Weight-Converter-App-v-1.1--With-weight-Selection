29bcd60ea4e2aa92ed0547b82830998d1c8f2f63
Nasser Jeary <36063816+jeary1988@users.noreply.github.com>
2018-09-09 11:39:02 -0700

    Update ViewController.swift

--- simple weight converter/ViewController.swift
+++ simple weight converter/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var button4: UIButton!
     
     
-    
     /*------------3--------------------*/
     @IBOutlet weak var pickerView: UIPickerView!
     
